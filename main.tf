694d724312990307d134d9a3a8d26556e077847b
Mateus Martins <mateusfm95@gmail.com>
2025-09-11 08:48:13 -0300

    infra: Terraform - Criação de bucket para armazenamento do estado remoto

--- main.tf
+++ main.tf
@@ -10,3 +10,24 @@ terraform {
 provider "aws" {
   region = "us-east-2"
 }
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket        = "nest-iac-state"
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Iac = "True"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = "nest-iac-state"
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
